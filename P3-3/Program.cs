3aad7962a070b271cf71c27bd254f25f7005fbe4
chelovek sverh <senior.nikita.adamovitch@ya.ru>
2023-03-03 22:56:22 +0500

    3 pz готова

---
+++ P3-3/Program.cs
@@ -0,0 +1,13 @@
+﻿using P3_3;
+
+BynaryTree tree = new BynaryTree(6);
+
+//BynaryTree.GetTreeData(tree.Root);
+Console.Write("Среднее значение:");
+BynaryTree.AverageArif(tree.Root);
+Console.WriteLine("Положительные числа: " + BynaryTree.CountPlus(tree.Root));
+Console.WriteLine("Отрицательные числа: " + BynaryTree.CountMinus(tree.Root));
+
+Console.Write("Введите число для поиска:");
+int sverka = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Количество вхождений вашего числа: " + BynaryTree.Countsverka(tree.Root, sverka));
